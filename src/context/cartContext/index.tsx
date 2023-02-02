bb4e8cb611dccb434360e2e91efbfad76c899564
1pedr1n <pedrinfnn@gmail.com>
2023-02-02 01:19:11 -0300

    clean code

--- src/context/cartContext/index.tsx
+++ src/context/cartContext/index.tsx
@@ -1,21 +1,11 @@
 import { createContext, ReactNode, useState, useEffect } from "react";
 import { IProductProps } from "@/interfaces/interface.product";
+import { ICart } from "@/interfaces/interface.cart";
+import { ICartProps } from "@/interfaces/interface.cardprops";
 type CartContextType = {
   children: ReactNode;
 };
 export const CartContext = createContext({} as ICartProps);
-
-export interface ICart {
-  product: IProductProps;
-  amount: number;
-}
-
-interface ICartProps {
-  totalPrice: number;
-  cart: ICart[];
-  addProduct: (product: IProductProps) => void;
-  removeProduct: (product: IProductProps, deleteAll?: boolean) => void;
-}
 
 export const CartProvider = ({ children }: CartContextType) => {
   const [cart, setCart] = useState<ICart[]>([]);
--- src/interfaces/interface.BoxProducts.ts
+++
@@ -1,9 +0,0 @@
-export interface BoxProductProps {
-  id: number;
-  name: string;
-  brand: string;
-  description: string;
-  price: number;
-  img: string;
-  click: () => void;
-}
